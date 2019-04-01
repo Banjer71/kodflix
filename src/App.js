be0acf270ffff5ea693da00871e92d9800083438
Banjer71 <davgio7@gmail.com>
2019-04-01 15:24:48 +0100

    add my favourite movie picture

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/starwars_cover.jpg'
+
+import  './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           
-            <h1>Welcome to the Kodflix</h1>
-             {/* Edit <code>src/App.js</code> and save to reload. */}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <img src={logo} alt='cover' />
       </div>
-    );
+    )
   }
 }
+
+
+
+
+
 
 export default App;
