3434346adcb5f833f25ca98c575f4216c287dbf1
Banjer71 <davgio7@gmail.com>
2019-04-01 16:43:33 +0100

    replace TV show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import logo from './img/starwars_cover.jpg'
+import mainPic from './img/starwars_cover.jpg'
 
+import goonies from './img/goonies.jpg'
+import brother from './img/brothers.jpg'
+import wargames from './img/wargames.jpg'
+import strangeThings from './img/strange_things.jpg'
+import firstMan from './img/first_man.jpg'
+import backtothefuture from './img/backtothefuture.jpg'
 import  './App.css';
 
 
@@ -9,7 +15,20 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <img src={logo} alt='cover' />
+        <img src={mainPic} alt='cover' />
+        
+      <div className='container'>
+        <div className= 'item'><img src={goonies} alt='cover' /></div>
+        <div className= 'item'><img src={brother} alt='cover' /></div>
+        <div className= 'item'><img src={wargames} alt='cover' /></div>
+      </div>
+
+      <div className='container'>
+        <div className= 'item'><img src={backtothefuture} alt='cover' /></div>
+        <div className= 'item'><img src={strangeThings} alt='cover' /></div>
+        <div className= 'item'><img src={firstMan} alt='cover' /></div>
+      </div>
+        
       </div>
     )
   }
