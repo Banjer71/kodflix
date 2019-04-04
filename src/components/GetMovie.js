37f9668cf7d85cc226360518fad9b88a001205e3
Banjer71 <davgio7@gmail.com>
2019-04-04 23:47:10 +0100

    #12 Move the covers data model to a proper front-end data source

---
+++ src/components/GetMovie.js
@@ -0,0 +1,19 @@
+import goonies from '../img/goonies.jpg';
+import brother from '../img/brothers.jpg';
+import wargames from '../img/wargames.jpg';
+import strangeThings from '../img/strange_things.jpg';
+import firstMan from '../img/first_man.jpg';
+import backtothefuture from '../img/backtothefuture.jpg';
+
+function GetMovie() {
+    return [
+      {id:'goonies', name:'The Goonies', cover: goonies},
+      {id:'brotherwht', name:'Brother Where are Thou', cover: brother},
+      {id:'warganmes', name:'The Goonies', cover: wargames},
+      {id:'backtothefuture', name:'Back To The Future', cover: backtothefuture},
+      {id:'strangethings', name:'Strange Things', cover: strangeThings},
+      {id:'firstman', name:'First Man', cover: firstMan}
+    ]
+  }
+
+export default GetMovie
--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,24 +1,23 @@
 import React from 'react';
 
 import Movie from './Movie';
+import getMovie from './GetMovie';
 
-import goonies from '../img/goonies.jpg';
-import brother from '../img/brothers.jpg';
-import wargames from '../img/wargames.jpg';
-import strangeThings from '../img/strange_things.jpg';
-import firstMan from '../img/first_man.jpg';
-import backtothefuture from '../img/backtothefuture.jpg';
+
 
 function MovieGrid() {
     return (
       <div>
       <div className='container'>
-            <Movie id='goonies' name='The Goonies' cover={goonies} />
-            <Movie id='brotherwht'name='Brother Where are Thou' cover={brother} />
-            <Movie id='warganmes'name='WarGames' cover={wargames} />
-            <Movie id='backtothefuture' name='Back To The Future' cover={backtothefuture} />
-            <Movie id='strangethings' name='Strange Things' cover={strangeThings} />
-            <Movie id='firstman' name='First Man' cover={firstMan} />
+            {
+              getMovie().map(movie => (
+                <Movie 
+                key={movie.id}
+                id={movie.id} 
+                name={movie.name} 
+                cover={movie.cover} />
+                ))
+            }
           </div>
       </div>
     )
