b25f60a4c2eb829c1a3e423e42b1c36c1019cdcb
Davide Naccarati <davgio7@gmail.com>
2019-04-07 11:11:55 +0100

    #13 Introduce state (#1)
    * #13 Introduce component state
    
    * #13 fix a little issue review with Ricardo about case-sensitive path
    
    * #13 removed unnecessary empty linse on Detail.js page
    
    * removed one unnecessary line and add onther one

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+class Details extends Component {
+    constructor() {
+        super();
+        this.state= {
+            welcomeMessage: 'Hello to the deatils page'
+        }
+    }
 
-function Details() {
-    return (
-        <div>
-            <h1>Hello to the deatils page</h1>
-            <Link to='/'>Back to the Homepage</Link>
-        </div>
-    )
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                welcomeMessage: 'Stay with us...the best is yet to come'
+            });
+        }, 2000);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.welcomeMessage}</h1>
+                <Link to='/'>Back to the Homepage</Link>
+            </div>
+        )
+    }
 }
-
 
 export default Details
