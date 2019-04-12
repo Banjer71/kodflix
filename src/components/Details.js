312731bbf44907337815f7e6abe41352a8ab7322
Banjer71 <davgio7@gmail.com>
2019-04-12 19:28:57 +0100

    #16 added synopsis and cover imagage to the detail movie page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import getMovie from './GetMovie';
+import './details.css';
 
 class Details extends Component {
     constructor() {
@@ -12,24 +13,29 @@ class Details extends Component {
 
     componentDidMount() {
         let movie_id = this.props.match.params.movie_id;
-        let getmovie = getMovie()
-            .find( (getmovie) => getmovie.id === movie_id);
+        let getmovie = getMovie().find((getmovie) => getmovie.id === movie_id);
         this.setState({ getmovie });
 
     }
 
     render() {
-        if(this.state.getmovie === undefined) {
+        if (this.state.getmovie === undefined) {
             return <Redirect to='/not-found' />
         } else {
             return (
                 <div>
                     <h1>{this.state.getmovie.name}</h1>
+                    <div className='container'>
+                        <div className='synopsis'>
+                            <p className='text'>{this.state.getmovie.synopsis}</p>
+                        </div>
+                        <img className='cover-image' src={this.state.getmovie.cover} alt={this.state.getmovie.name} />   
+                    </div>
                     <Link to='/'>Back to the Homepage</Link>
                 </div>
             )
         }
-        
+
     }
 }
 
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
-function NotFound() {
+function notFound() {
     return (
         <div>
-            <h1>404 Page not found!...Welcome to the most wonderful page you never found
-            </h1>
+            <h1>404 Page not found!...</h1>
+            <h1>Welcome to the most wonderful page you will never found :)</h1>
+            <Link to='./'><h3 className='tet'>Back to the Homepage!</h3></Link>
         </div>
     )
 }
 
-export default NotFound
+export default notFound
